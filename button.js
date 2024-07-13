662a2cee9cf9c39bfb91368152f1efa5926051df
fulaytar <fulaytar2405@gmail.com>
2024-07-13 21:13:29 +0300

    Update button.js

--- button.js
+++ button.js
@@ -3,9 +3,17 @@ document.addEventListener('DOMContentLoaded', function () {
   const orderButton = document.querySelector('.main-wr-element-a-button');
   const closeButton = document.querySelector('.button-modal-window');
 
+  function closeOverlayOnOutsideClick(event) {
+    if (event.target === backdrop) {
+      closeBackdrop();
+      backdrop.removeEventListener('click', closeOverlayOnOutsideClick);
+    }
+  }
+
   // Функція для відкриття бекдропа
   function openBackdrop() {
     backdrop.classList.add('is-open');
+    backdrop.addEventListener('click', closeOverlayOnOutsideClick);
   }
 
   // Функція для закриття бекдропа
